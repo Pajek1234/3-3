8af6887307556446c6b0ebb2f2b86f698ffe079e
Pajek1234 <162458983+Pajek1234@users.noreply.github.com>
2024-04-10 13:34:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int rozmiar = 11;
+    for (int i = 0; i < 11; i++) {
+      for (int j = 0; j < 11; j++) {
+        System.out.print(i * j);
+      System.out.print(" \t");
+      }
+      System.out.println();
+    }
+  }
+}
